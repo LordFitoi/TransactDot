40363a44919cc1c5a255a69c1c24bcbcd05dd7eb
Nahum Santana <nahumsantanam@gmail.com>
2023-10-15 13:55:32 -0400

    feat: aliases configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "http://localhost:8888/",
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+      "%": path.resolve(__dirname, "./public"),
+    }
+  }
 })
